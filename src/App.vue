1b920d5361e128315460b713ff8e42c56854d3f6
Lonely5490624 <l5490624@126.com>
2021-10-12 17:51:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+
+<template>
+
+    <swiper :effect="'cards'" :grabCursor="true" class="mySwiper">
+  <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+    
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import "swiper/css/effect-cards"
+
+import './style.css';
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  EffectCards
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([EffectCards]);
+
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+        
+        
+    };
+  },
+  methods: {
+    
+  }
+  
+}
+</script>
